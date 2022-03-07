a76bc16ec1c02e5c19e64143b48ad3c0df149e4a
Luca <luca.panzavolta@veepee.comd>
2022-03-07 20:53:07 +0100

    test: add tests for flatten()

--- src/flatten/flatten.test.ts
+++ src/flatten/flatten.test.ts
@@ -1,3 +1,26 @@
-describe("Jest", () => {
-  it("should be able to run", () => {});
+import { flatten } from "./flatten";
+
+describe("flatten()", () => {
+  it("should work with empty array", () => {
+    expect(flatten([])).toEqual([]);
+  });
+
+  it("should work with nested empty arrays", () => {
+    expect(flatten([[[]]])).toEqual([]);
+  });
+
+  it("should work with mono-dimensional array", () => {
+    const array = [1, 2, 3, 4, 5];
+    expect(flatten(array)).toEqual(array);
+  });
+
+  it("should work with bi-dimensional array", () => {
+    const expected = [1, 2, 3, 4, 5];
+    expect(flatten([1, [2, 3], [4, 5]])).toEqual(expected);
+  });
+
+  it("should work with n-dimensional array", () => {
+    const expected = [1, 2, 3, 4, 5, 6, 7, 8];
+    expect(flatten([1, [2, [3, 4, [5, [6, 7]]]], 8])).toEqual(expected);
+  });
 });
---
+++ src/flatten/flatten.ts
@@ -0,0 +1,3 @@
+export const flatten = (array: any) => {
+  return;
+};
