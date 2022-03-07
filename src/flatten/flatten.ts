812761afe13edbb77b2bb04b2e89804a21ac6a99
Luca <luca.panzavolta@veepee.comd>
2022-03-07 21:02:16 +0100

    feat: implement flatten()

--- src/flatten/flatten.ts
+++ src/flatten/flatten.ts
@@ -1,3 +1,9 @@
-export const flatten = (array: any) => {
-  return;
+export const flatten = (array: any[]): any[] => {
+  return array.reduce((accumulator: any[], element) => {
+    if (Array.isArray(element)) {
+      return accumulator.concat(flatten(element));
+    } else {
+      return [...accumulator, element];
+    }
+  }, []);
 };
